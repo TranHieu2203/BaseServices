eb6cc4973f1350593a4b3f79afecab4c283d245d
TranHieu2203 <49856578+TranHieu2203@users.noreply.github.com>
2022-08-04 10:04:10 +0700

    init Service/Interface/IProjectService.cs

---
+++ Service/Interface/IProjectService.cs
@@ -0,0 +1,17 @@
+﻿using Service.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Interface
+{
+    public interface IProjectService
+    {
+        Task<IEnumerable<ProjectDTO>> GetProjectAsync();
+        Task<ProjectDTO> GetById(int id);
+        Task<bool> InsertAsync(ProjectDTO projectDTO);
+        Task<int> CompletedAsync();
+    }
+}
